cd6dfd563d50739b49151dc82831ba8188f3dc45
IceInTheFire <392587201@qq.com>
2018-10-18 21:56:31 +0800

    提交配置

---
+++ config/sql.js
@@ -0,0 +1,10 @@
+module.exports = {
+    host     : '127.0.0.1',
+    port     : '3306',
+    user     : 'root',
+    password : '',
+    database : 'timotaoshu',
+    multipleStatements : true,  //是否允许执行多条sql语句
+    // insecureAuth: true,   //加入此项可解决此错误！！！  //使用旧（不安全）的连接方式去连接MySQL  远程连接
+    // debug: true,            //是否把连接情况打印到文件里
+};
