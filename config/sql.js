65ae134ff9a5f42b6febe187b1c958855836c045
IceInTheFire <392587201@qq.com>
2018-12-18 23:54:42 +0800

    '提交'

--- config/sql.js
+++ config/sql.js
@@ -2,7 +2,7 @@ module.exports = {
     host     : '127.0.0.1',
     port     : '3306',
     user     : 'root',
-    password : 'mlp123456',
+    password : '',
     database : 'timotaoshu',
     multipleStatements : true,  //是否允许执行多条sql语句
     // insecureAuth: true,   //加入此项可解决此错误！！！  //使用旧（不安全）的连接方式去连接MySQL  远程连接
