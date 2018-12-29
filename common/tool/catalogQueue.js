c0bdcfcbd1476c79cde16c1a24bd61feeeb7f177
IceInTheFire <392587201@qq.com>
2018-12-29 14:47:01 +0800

    提交代码

--- common/tool/catalogQueue.js
+++ common/tool/catalogQueue.js
@@ -29,6 +29,7 @@ catalogQueue.saturated = function() {
 }
 catalogQueue.drain = function() {
     console.log("所有任务执行完毕");
+    global.isReptile = false;       //这里是紧急修复bug，。问题是获取小说的json时，没有章节，然后导致爬书的时候，数量不对。到时候需要优化
     // console.log("当所有任务都执行完时，会调用drain函数");
 }
 
