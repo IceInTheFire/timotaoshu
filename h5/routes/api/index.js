fd76a045486bc56b7786d753e8465def234660e5
IceInTheFire <392587201@qq.com>
2019-01-05 14:33:00 +0800

    更改ip

--- h5/routes/api/index.js
+++ h5/routes/api/index.js
@@ -6,7 +6,7 @@ const {log, tool} = require("../../tool/require");
 router.use('', function(req, res, next) {
     // tool.allowVisit(res); //为了跨域
   // res.send('你好，欢迎来到提莫淘书h5接口文档世界<br><a href="http://localhost:1111/api">接口文档地址</a>');
-    res.render('api/index', { title: '提莫淘书接口文档', href:'http://106.12.37.138:1111/_book/'});
+    res.render('api/index', { title: '提莫淘书接口文档', href:'http://localhost:1111/_book/'});
 });
 
 module.exports = router;
